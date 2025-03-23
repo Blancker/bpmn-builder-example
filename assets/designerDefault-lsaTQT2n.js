3fba8638f57eaf11424e8a3f8b0d7a180b571cdd
xiekaipeng <blancker@163.com>
2025-03-23 22:54:02 +0800

    初始化仓库

---
+++ assets/designerDefault-lsaTQT2n.js
@@ -0,0 +1,1 @@
+import{r as c,c as s,o as _}from"./index-BTJhmVFW.js";const a=(n,o)=>{const e=n.__vccOpts||n;for(const[t,r]of o)e[t]=r;return e},p={};function f(n,o){const e=c("bpmn-designer");return _(),s(e,{autoInit:"",border:""})}const i=a(p,[["render",f]]);export{i as default};
